cf1001ffc4bd65a318d13ba23d822b5ccd65dde6
akash18501 <akash24nov2000@gmail.com>
2022-08-10 11:12:34 +0530

    data updated

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -15,28 +15,43 @@ INSERT INTO book_user VALUES (2,2);
 INSERT INTO book_user VALUES (3,1);
 INSERT INTO book_user VALUES (3,2);
 
-INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (1,5,'completed',100,'buy','2019-12-01','2021-01-01');
-INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (2,10,'incomplete',200,'sell','2019-11-01','2021-02-01');
-INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (3,15,'in-progess',300,'buy','2019-10-01','2021-03-01');
-INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (4,20,'completed',400,'sell','2019-09-01','2021-04-01');
-INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (5,25,'incomplete',500,'buy','2019-08-01','2021-05-01');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (1,5,'Success',100,'buy','2022-05-01','2022-05-04');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (2,10,'Success',200,'sell','2022-05-04','2022-05-06');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (3,15,'Success',300,'buy','2022-06-05','2022-06-07');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (4,20,'Fail',400,'sell','2022-07-11','2022-07-13');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (5,25,'Success',500,'buy','2022-07-21','2022-07-22');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (6,5,'Success',100,'buy','2022-05-01','2022-05-04');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (7,10,'Success',200,'sell','2022-05-04','2022-05-06');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (8,15,'Success',300,'buy','2022-06-05','2022-06-07');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (9,20,'Fail',400,'sell','2022-07-11','2022-07-13');
+INSERT INTO trades (id,quantity,status,price,BUYSELL,TRADEDATE,SETTLEMENTDATE) VALUES (10,25,'Success',500,'buy','2022-07-21','2022-07-22');
 
-INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status,user_user_id) VALUES (8,'abc','hef','rahul','2019-12-01','THANKYOU200','ASDD',100,'MATURED',2);
-INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status,user_user_id) VALUES (9,'def','rhe','aakash','2019-11-01','THANKYOU100','ASDD',200,'IMMATURE',2);
-INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status,user_user_id) VALUES (3,'feg','fjt','aastha','2019-10-01','THANKYOU400','ASDD',300,'MATURED',3);
-INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status,user_user_id) VALUES (4,'grd','ijo','aakarshi','2019-09-01','THANKYOU700','ASDD',400,'IMMATURE',4);
-INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (5,'teg','opl','chirag','2019-08-01','THANKYOU200','ASDD',500,'MATURED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (8,'US6949181045','337833100','Microsoft','2022-12-01','7.12','Corporate',100,'NOT REDEEMED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (9,'US7947261045','265765100','Alphabet','2022-08-05','8.01','Corporate',200,'NOT REDEEMED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status,user_user_id) VALUES (10,'US7946687045','989599100','Apple','2022-07-25','7.89','Corporate',300,'REDEEMED',3);
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status,user_user_id) VALUES (11,'US9978691045','876311100','Amazon','2023-02-10','8.18','Corporate',400,'NOT REDEEMED',4);
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (12,'US4999281045','280986100','Walmart','2023-04-20','9.00','Corporate',500,'NOT REDEEMED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (13,'US8947781045','244325400','Deutsche Bank','2022-08-12','8.81','Corporate',500,'NOT REDEEMED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (14,'US2942345045','332789600','Goldman Sachs','2022-06-09','7.69','Corporate',500,'REDEEMED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (15,'US2941234045','142756790','Myntra','2022-07-30','7.81','Corporate',900,'NOT REDEEMED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (16,'US2946789045','432788760','AirBnb','2022-08-02','8.89','Corporate',300,'NOT REDEEMED');
+INSERT INTO securities (id,ISIN,CUSIP,issuer,maturityDate,coupon,type,faceValue,status) VALUES (17,'US5944562045','782789908','GoPro','2023-05-09','8.91','Corporate',400,'REDEEMED');
 
 
 INSERT INTO counter_party values (1,'counterparty1');
 INSERT INTO counter_party values (2,'counterparty2');
 
 -- book_id,security_id,counterparty_id
-update trades set book_book_id=1,security_id=3,counter_party_counter_party_id=1 where id=1;
-update trades set book_book_id=1,security_id=4,counter_party_counter_party_id=1 where id=2;
-update trades set book_book_id=2,security_id=3,counter_party_counter_party_id=2 where id=3;
-update trades set book_book_id=3,security_id=3,counter_party_counter_party_id=1 where id=4;
-update trades set book_book_id=2,security_id=3,counter_party_counter_party_id=2 where id=5;
+update trades set book_book_id=1,security_id=8,counter_party_counter_party_id=1 where id=1;
+update trades set book_book_id=1,security_id=9,counter_party_counter_party_id=1 where id=2;
+update trades set book_book_id=2,security_id=8,counter_party_counter_party_id=2 where id=3;
+update trades set book_book_id=3,security_id=8,counter_party_counter_party_id=1 where id=4;
+update trades set book_book_id=2,security_id=8,counter_party_counter_party_id=2 where id=5;
+update trades set book_book_id=1,security_id=9,counter_party_counter_party_id=1 where id=6;
+update trades set book_book_id=1,security_id=10,counter_party_counter_party_id=1 where id=7;
+update trades set book_book_id=2,security_id=10,counter_party_counter_party_id=2 where id=8;
+update trades set book_book_id=3,security_id=11,counter_party_counter_party_id=1 where id=9;
+update trades set book_book_id=2,security_id=12,counter_party_counter_party_id=2 where id=10;
 
 
 
