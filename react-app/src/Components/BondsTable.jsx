acd2a57d5be437b805a7402aff5c259f5c622c8a
Adesh <adesh.attavar@gmail.com>
2022-08-10 11:58:39 +0530

    Added colour to maturity date

--- react-app/src/Components/BondsTable.jsx
+++ react-app/src/Components/BondsTable.jsx
@@ -1,13 +1,13 @@
 import React, { useMemo, useState, useEffect } from "react";
 import MaterialReactTable from "material-react-table";
 import {
-  Box,
-  Button,
-  ListItemIcon,
-  MenuItem,
-  Typography,
-  Alert,
-  LinearProgress,
+    Box,
+    Button,
+    ListItemIcon,
+    MenuItem,
+    Typography,
+    Alert,
+    LinearProgress,
 } from "@mui/material";
 import { AccountCircle, Send } from "@mui/icons-material";
 // import { makeData } from "./makeData";
@@ -17,134 +17,161 @@ import { getAllSecurities, addToWatchList } from "../api/api";
 import { CircularProgress } from "@mui/material";
 
 const BondsTable = () => {
-  const [securities, setSecurities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [addedToWatchList, setAddedToWatchList] = useState(false);
+    const [securities, setSecurities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [addedToWatchList, setAddedToWatchList] = useState(false);
 
-  useEffect(() => {
-    console.log("call to api");
+    useEffect(() => {
+        console.log("call to api");
 
-    getAllSecurities().then((data) => {
-      setSecurities(data);
-    });
-  }, []);
+        getAllSecurities().then((data) => {
+            setSecurities(data);
+        });
+    }, []);
 
-  useEffect(() => {
-    console.log("securities in table", securities);
-  }, [securities]);
+    useEffect(() => {
+        console.log("securities in table", securities);
+    }, [securities]);
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "isin", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "ISIN",
-        size: 100,
-      },
-      {
-        accessorKey: "cusip", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "CUSIP",
-        size: 100,
-      },
-      {
-        accessorKey: "issuer", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Issuer",
-        size: 100,
-      },
-      {
-        accessorKey: "maturityDate", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Maturity Date",
-        size: 100,
-      },
-      {
-        accessorKey: "coupon", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Coupon",
-        size: 100,
-      },
-      {
-        accessorKey: "type", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Type",
-        size: 100,
-      },
-      {
-        accessorKey: "faceValue", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Face Value",
-        size: 100,
-      },
-    ],
-    []
-  );
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: "isin", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "ISIN",
+                size: 100,
+            },
+            {
+                accessorKey: "cusip", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "CUSIP",
+                size: 100,
+            },
+            {
+                accessorKey: "issuer", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Issuer",
+                size: 100,
+            },
+            {
+                accessorKey: "maturityDate", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Maturity Date",
+                size: 100,
+                Cell: ({ cell }) => (
+                    <Box
+                        sx={(theme) => ({
+                            backgroundColor:
+                                new Date(cell.getValue()) < new Date()
+                                    ? theme.palette.error.dark
+                                    : new Date(cell.getValue()).getMonth() ==
+                                          new Date().getMonth() ||
+                                      new Date(cell.getValue()).getMonth() ==
+                                          new Date().getMonth() + 1
+                                    ? theme.palette.warning.main
+                                    : theme.palette.success.main,
+                            borderRadius: "0.25rem",
+                            color: "#fff",
+                            p: "0.65rem",
+                        })}
+                    >
+                        {cell.getValue()?.toLocaleString?.("en-US", {
+                            style: "currency",
+                            currency: "USD",
+                            minimumFractionDigits: 0,
+                            maximumFractionDigits: 0,
+                        })}
+                    </Box>
+                ),
+            },
+            {
+                accessorKey: "coupon", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Coupon",
+                size: 100,
+            },
+            {
+                accessorKey: "type", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Type",
+                size: 100,
+            },
+            {
+                accessorKey: "faceValue", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Face Value",
+                size: 100,
+            },
+        ],
+        []
+    );
 
-  return (
-    <>
-      <ResponsiveAppBar />
-      {addedToWatchList === true ? (
-        <Alert severity="success">
-          This is a success alert — check it out!
-        </Alert>
-      ) : (
-        <span></span>
-      )}
-      <Typography variant="h3" component="h2" m={3}>
-        Bonds Experiment
-      </Typography>
-      {securities.length > 0 ? (
-        <MaterialReactTable
-          columns={columns}
-          data={securities}
-          enableClickToCopy
-          enableColumnFilterChangeMode
-          enableColumnOrdering
-          enableColumnResizing
-          // enableEditing
-          enableGrouping
-          enablePinning
-          // enableRowActions
-          // enableRowNumbers
-          enableRowSelection
-          muiSelectCheckboxProps={({ row }) => ({
-            disabled: row.original.user != null,
-          })}
-          // onEditRowSubmit={handleSaveRow}
-          positionToolbarAlertBanner="bottom"
-          renderDetailPanel={({ row }) => {
-            return <TradeTable tradeData={row.original.trades} />;
-          }}
-          renderTopToolbarCustomActions={({ table }) => {
-            const handleAddToTheBook = () => {
-              const selectedRows = table.getSelectedRowModel().flatRows;
-              const securities_ids = selectedRows.map((e, i) => {
-                return e.original.id;
-              });
-              console.log("selected rows", securities_ids);
-              addToWatchList(securities_ids, 2)
-                .then((res) => {
-                  setAddedToWatchList(true);
-                  setTimeout(() => {
-                    setAddedToWatchList(false);
-                  }, 2000);
-                })
-                .catch((error) => {});
-            };
+    return (
+        <>
+            <ResponsiveAppBar />
+            {addedToWatchList === true ? (
+                <Alert severity="success">Bonds added to the watchlist</Alert>
+            ) : (
+                <span></span>
+            )}
+            <Typography variant="h3" component="h2" m={3}>
+                Bonds
+            </Typography>
+            {securities.length > 0 ? (
+                <MaterialReactTable
+                    columns={columns}
+                    data={securities}
+                    enableClickToCopy
+                    enableColumnFilterChangeMode
+                    enableColumnOrdering
+                    enableColumnResizing
+                    // enableEditing
+                    enableGrouping
+                    enablePinning
+                    // enableRowActions
+                    // enableRowNumbers
+                    enableRowSelection
+                    muiSelectCheckboxProps={({ row }) => ({
+                        disabled: row.original.user != null,
+                    })}
+                    // onEditRowSubmit={handleSaveRow}
+                    positionToolbarAlertBanner="bottom"
+                    renderDetailPanel={({ row }) => {
+                        return <TradeTable tradeData={row.original.trades} />;
+                    }}
+                    renderTopToolbarCustomActions={({ table }) => {
+                        const handleAddToTheBook = () => {
+                            const selectedRows =
+                                table.getSelectedRowModel().flatRows;
+                            const securities_ids = selectedRows.map((e, i) => {
+                                return e.original.id;
+                            });
+                            console.log("selected rows", securities_ids);
+                            addToWatchList(securities_ids, 3)
+                                .then((res) => {
+                                    setAddedToWatchList(true);
+                                    setTimeout(() => {
+                                        setAddedToWatchList(false);
+                                    }, 2000);
+                                })
+                                .catch((error) => {});
+                        };
 
-            return (
-              <div style={{ display: "flex", gap: "0.5rem" }}>
-                <Button
-                  color="info"
-                  disabled={table.getSelectedRowModel().flatRows.length === 0}
-                  onClick={handleAddToTheBook}
-                  variant="contained"
-                >
-                  Add to the Watchlist
-                </Button>
-              </div>
-            );
-          }}
-        />
-      ) : (
-        <LinearProgress color="primary" />
-      )}
-    </>
-  );
+                        return (
+                            <div style={{ display: "flex", gap: "0.5rem" }}>
+                                <Button
+                                    color="info"
+                                    disabled={
+                                        table.getSelectedRowModel().flatRows
+                                            .length === 0
+                                    }
+                                    onClick={handleAddToTheBook}
+                                    variant="contained"
+                                >
+                                    Add to the Watchlist
+                                </Button>
+                            </div>
+                        );
+                    }}
+                />
+            ) : (
+                <LinearProgress color="primary" />
+            )}
+        </>
+    );
 };
 
 export default BondsTable;
--- react-app/src/Components/WatchList.jsx
+++ react-app/src/Components/WatchList.jsx
@@ -1,12 +1,12 @@
 import React, { useMemo, useState, useEffect } from "react";
 import MaterialReactTable from "material-react-table";
 import {
-  Box,
-  Button,
-  LinearProgress,
-  ListItemIcon,
-  MenuItem,
-  Typography,
+    Box,
+    Button,
+    LinearProgress,
+    ListItemIcon,
+    MenuItem,
+    Typography,
 } from "@mui/material";
 import { AccountCircle, Send } from "@mui/icons-material";
 // import { makeData } from "./makeData";
@@ -15,116 +15,141 @@ import TradeTable from "./TradeTable";
 import { getWatchList } from "../api/api";
 
 const WatchList = () => {
-  const [watchlist, setWatchList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+    const [watchlist, setWatchList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    console.log("call to api");
+    useEffect(() => {
+        console.log("call to api");
 
-    getWatchList(2).then((data) => {
-      setWatchList(data);
-    });
-  }, []);
+        getWatchList(3).then((data) => {
+            setWatchList(data);
+        });
+    }, []);
 
-  useEffect(() => {
-    console.log("securities in table", watchlist);
-  }, [watchlist]);
+    useEffect(() => {
+        console.log("securities in table", watchlist);
+    }, [watchlist]);
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "isin", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "ISIN",
-        size: 100,
-      },
-      {
-        accessorKey: "cusip", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "CUSIP",
-        size: 100,
-      },
-      {
-        accessorKey: "issuer", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Issuer",
-        size: 100,
-      },
-      {
-        accessorKey: "maturityDate", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Maturity Date",
-        size: 100,
-      },
-      {
-        accessorKey: "coupon", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Coupon",
-        size: 100,
-      },
-      {
-        accessorKey: "type", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Type",
-        size: 100,
-      },
-      {
-        accessorKey: "faceValue", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-        header: "Face Value",
-        size: 100,
-      },
-    ],
-    []
-  );
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: "isin", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "ISIN",
+                size: 100,
+            },
+            {
+                accessorKey: "cusip", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "CUSIP",
+                size: 100,
+            },
+            {
+                accessorKey: "issuer", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Issuer",
+                size: 100,
+            },
+            {
+                accessorKey: "maturityDate", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Maturity Date",
+                size: 100,
+                Cell: ({ cell }) => (
+                    <Box
+                        sx={(theme) => ({
+                            backgroundColor:
+                                new Date(cell.getValue()) < new Date()
+                                    ? theme.palette.error.dark
+                                    : new Date(cell.getValue()).getMonth() ==
+                                          new Date().getMonth() ||
+                                      new Date(cell.getValue()).getMonth() ==
+                                          new Date().getMonth() + 1
+                                    ? theme.palette.warning.main
+                                    : theme.palette.success.main,
+                            borderRadius: "0.25rem",
+                            color: "#fff",
+                            p: "0.65rem",
+                        })}
+                    >
+                        {cell.getValue()?.toLocaleString?.("en-US", {
+                            style: "currency",
+                            currency: "USD",
+                            minimumFractionDigits: 0,
+                            maximumFractionDigits: 0,
+                        })}
+                    </Box>
+                ),
+            },
+            {
+                accessorKey: "coupon", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Coupon",
+                size: 100,
+            },
+            {
+                accessorKey: "type", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Type",
+                size: 100,
+            },
+            {
+                accessorKey: "faceValue", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                header: "Face Value",
+                size: 100,
+            },
+        ],
+        []
+    );
 
-  return (
-    <>
-      <ResponsiveAppBar />
-      <Typography variant="h3" component="h2" m={3}>
-        Watchlist
-      </Typography>
-      {watchlist.length > 0 ? (
-        <MaterialReactTable
-          columns={columns}
-          data={watchlist}
-          enableClickToCopy
-          enableColumnFilterChangeMode
-          enableColumnOrdering
-          enableColumnResizing
-          // enableEditing
-          enableGrouping
-          enablePinning
-          // enableRowActions
-          // enableRowNumbers
-          // enableRowSelection
-          // muiSelectCheckboxProps={({ row }) => ({
-          //   disabled: row.getValue("salary") < 60000,
-          // })}
-          // onEditRowSubmit={handleSaveRow}
-          positionToolbarAlertBanner="bottom"
-          renderDetailPanel={({ row }) => {
-            return <TradeTable tradeData={row.original.trades} />;
-          }}
-          // renderTopToolbarCustomActions={({ table }) => {
-          //   const handleAddToTheBook = () => {
-          //     table.getSelectedRowModel().flatRows.map((row) => {
-          //       alert("contact " + row.getValue("name"));
-          //     });
-          //   };
+    return (
+        <>
+            <ResponsiveAppBar />
+            <Typography variant="h3" component="h2" m={3}>
+                Watchlist
+            </Typography>
+            {watchlist.length > 0 ? (
+                <MaterialReactTable
+                    columns={columns}
+                    data={watchlist}
+                    enableClickToCopy
+                    enableColumnFilterChangeMode
+                    enableColumnOrdering
+                    enableColumnResizing
+                    // enableEditing
+                    enableGrouping
+                    enablePinning
+                    // enableRowActions
+                    // enableRowNumbers
+                    // enableRowSelection
+                    // muiSelectCheckboxProps={({ row }) => ({
+                    //   disabled: row.getValue("salary") < 60000,
+                    // })}
+                    // onEditRowSubmit={handleSaveRow}
+                    positionToolbarAlertBanner="bottom"
+                    renderDetailPanel={({ row }) => {
+                        return <TradeTable tradeData={row.original.trades} />;
+                    }}
+                    // renderTopToolbarCustomActions={({ table }) => {
+                    //   const handleAddToTheBook = () => {
+                    //     table.getSelectedRowModel().flatRows.map((row) => {
+                    //       alert("contact " + row.getValue("name"));
+                    //     });
+                    //   };
 
-          //   return (
-          //     <div style={{ display: "flex", gap: "0.5rem" }}>
-          //       <Button
-          //         color="info"
-          //         disabled={table.getSelectedRowModel().flatRows.length === 0}
-          //         onClick={handleAddToTheBook}
-          //         variant="contained"
-          //       >
-          //         Add to the book
-          //       </Button>
-          //     </div>
-          //   );
-          // }}
-        />
-      ) : (
-        <LinearProgress color="primary" />
-      )}
-    </>
-  );
+                    //   return (
+                    //     <div style={{ display: "flex", gap: "0.5rem" }}>
+                    //       <Button
+                    //         color="info"
+                    //         disabled={table.getSelectedRowModel().flatRows.length === 0}
+                    //         onClick={handleAddToTheBook}
+                    //         variant="contained"
+                    //       >
+                    //         Add to the book
+                    //       </Button>
+                    //     </div>
+                    //   );
+                    // }}
+                />
+            ) : (
+                <LinearProgress color="primary" />
+            )}
+        </>
+    );
 };
 
 export default WatchList;
